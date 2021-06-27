aa6bc29974abe34edf4d53ce90abb510ad75f330
Chun Wu <the.chun.wu@gmail.com>
2021-06-27 12:21:23 -0400

    Puzzle: Find the Missing Element

--- _Find the Missing Element/FindTheMissingElement.py
+++ _Find the Missing Element/FindTheMissingElement.py
@@ -51,7 +51,7 @@ def finder3(arr1, arr2):
     # Perform an XOR between the numbers in the arrays
     for num in arr1+arr2: 
         result^=num 
-        print result
+        print (result)
         
     return result 
 
