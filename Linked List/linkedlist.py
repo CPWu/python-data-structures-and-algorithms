7eb60ff31d75814f7bdf76320385380873e6fbe4
Chun Wu <the.chun.wu@gmail.com>
2021-06-28 11:34:57 -0400

    Cleaning up Linked List Section/

--- Linked List/linkedlist.py
+++ Linked List/linkedlist.py
@@ -1,15 +1,20 @@
+# Node Class
 class Node(object):
     def __init__(self,value):
         self.value = value
         self.nextNode = None
 
+# Instantiate 3 Instances of Node
 a = Node(1)
 b = Node(2)
 c = Node(3)
 
+# Set the 'pointers' of the nodes
 a.nextNode = b
 b.nextNode = c
 
+# Print value of Node a
 print(a.value)
 
+# Print the value of the node that node 'a' is pointing to
 print(a.nextNode.value)
