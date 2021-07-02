220be54cf839ce8a28fedcfea4986b834ec16060
Chun Wu <the.chun.wu@gmail.com>
2021-07-02 10:58:04 -0400

    Modified Doubly Linked List to not use Lists

--- Doubly-Linked List/doublylinkedlist.py
+++ Doubly-Linked List/doublylinkedlist.py
@@ -1,17 +1,64 @@
-class DoublyLinkedListNode(object):
-    def __init__(self,value):
-        self.value = value
-        self.next_node = None
-        self.previous_node = None
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        # if Doubly Linked List is empty
+        if self.head is None:
+            new_node.prev = None
+            self.head = new_node
+        else:
+            current_node = self.head
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+            new_node.prev = current_node
+            new_node.next = None
 
 
+    def prepend(self, data):
+        new_node = Node(data)
 
-a = DoublyLinkedListNode(1)
-b = DoublyLinkedListNode(2)
-c = DoublyLinkedListNode(3)
+        # Prepend onto an empty list
+        if self.head is None:
+            new_node.prev = None
+            self.head = new_node
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            new_node.prev = None
 
+    def print(self):
+        current_node = self.head
 
-a.next_node = b
-b.next_node = a
-b.previous_node = a
-c.previous_node = b
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def add_node(self):
+        pass
+
+    def delete_node(self):
+        pass
+
+    def reverse(self):
+        pass
+
+    def remove_dupe(self):
+        pass
+    
+doublyLinkedList = DoublyLinkedList()
+doublyLinkedList.prepend("0")
+doublyLinkedList.append("A")
+doublyLinkedList.append("B")
+doublyLinkedList.append("C")
+doublyLinkedList.prepend("5")
+doublyLinkedList.print()
