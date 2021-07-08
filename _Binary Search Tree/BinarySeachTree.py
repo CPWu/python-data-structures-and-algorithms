2a662ff44f9dd6425f0e0215573bc12401878ab2
Chun Wu <the.chun.wu@gmail.com>
2021-07-07 23:14:10 -0400

    BST Check

--- _Binary Search Tree/BinarySeachTree.py
+++ _Binary Search Tree/BinarySeachTree.py
@@ -31,7 +31,17 @@ class BST(object):
                 self._insert(data, current_node.right)
         else:
             print ("Value is already present in tree.")
-    
+
+    def inorder_print_tree(self):
+        if self.root:
+            self._inorder_print_tree(self.root)
+
+    def _inorder_print_tree(self, cur_node):
+        if cur_node:
+            self._inorder_print_tree(cur_node.left)
+            print(str(cur_node.data))
+            self._inorder_print_tree(cur_node.right)
+        
     def find(self, data):
         if self.root:
             is_found = self._find(data, self.root)
@@ -49,13 +59,39 @@ class BST(object):
         if data == current_node.data:
             return True
 
+    def is_bst_satisfied(self):
+        def helper(node, lower=float('-inf'), upper=float('inf')):
+            if not node:
+                return True
+            
+            val = node.data
+            if val <= lower or val >= upper:
+                return False
+
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(self.root)
+
 bst = BST()
 bst.insert(4)
 bst.insert(2)
 bst.insert(8)
 bst.insert(5)
 bst.insert(10)
-print(bst.find(4))
-print(bst.find(4))
-print(bst.find(11))
+
+tree = BST()
+tree.root = Node(1)
+tree.root.left = Node(-4)
+tree.root.right = Node(3)
+tree.root.left.left = Node(-5)
+tree.root.left.right = Node(0)
+tree.root.right.left = Node(2)
+tree.root.right.right = Node(7)
+tree.root.right.right.right = Node(-8)
+
+print(tree.is_bst_satisfied())
     
