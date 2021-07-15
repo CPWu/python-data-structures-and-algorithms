0bfce1f4d308e9a410c9c644ec47009967de8cc0
Chun Wu <the.chun.wu@gmail.com>
2021-07-15 09:11:37 -0400

    Puzzle: Nested List Weight Sum II

--- _Nested List Weight Sum II/NestedListWeightSumII.py
+++ _Nested List Weight Sum II/NestedListWeightSumII.py
@@ -1,28 +1,3 @@
-# class Solution:
-# 	def depthSumInverse(self, nestedList):
-# 			sol = []
-# 			def dfs(curr, depth) -> None:
-# 				if depth > len(sol):
-# 					sol.append(0)
-					
-# 				if curr.getInteger():
-# 					sol[depth - 1] += curr.getInteger()
-# 					return
-# 				for i in curr.getList():
-# 					dfs(i, depth + 1)
-			
-# 			for i in nestedList:
-# 				dfs(i, 1)
-			
-# 			max_depth = len(sol)
-# 			for i in range(max_depth):
-# 				sol[i] = (max_depth - i) * sol[i]
-				
-# 			return sum(sol)
-
-
-# solution = Solution()
-
 # Nested List Weight Sum II
 # Time(D) D = Depth of Nested Lists
 # Time(N) N = Number of items in List
