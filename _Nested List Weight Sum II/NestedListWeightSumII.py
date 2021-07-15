e04ec53e184149df8b6fbd4ac5ef38a9f438eda8
Chun Wu <the.chun.wu@gmail.com>
2021-07-15 09:10:44 -0400

    Puzzle: Shortest Word Distance II

--- _Nested List Weight Sum II/NestedListWeightSumII.py
+++ _Nested List Weight Sum II/NestedListWeightSumII.py
@@ -1,24 +1,49 @@
-class Solution:
-	def depthSumInverse(self, nestedList):
-			sol = []
-			def dfs(curr, depth) -> None:
-				if depth > len(sol):
-					sol.append(0)
+# class Solution:
+# 	def depthSumInverse(self, nestedList):
+# 			sol = []
+# 			def dfs(curr, depth) -> None:
+# 				if depth > len(sol):
+# 					sol.append(0)
 					
-				if curr.getInteger():
-					sol[depth - 1] += curr.getInteger()
-					return
-				for i in curr.getList():
-					dfs(i, depth + 1)
+# 				if curr.getInteger():
+# 					sol[depth - 1] += curr.getInteger()
+# 					return
+# 				for i in curr.getList():
+# 					dfs(i, depth + 1)
 			
-			for i in nestedList:
-				dfs(i, 1)
+# 			for i in nestedList:
+# 				dfs(i, 1)
 			
-			max_depth = len(sol)
-			for i in range(max_depth):
-				sol[i] = (max_depth - i) * sol[i]
+# 			max_depth = len(sol)
+# 			for i in range(max_depth):
+# 				sol[i] = (max_depth - i) * sol[i]
 				
-			return sum(sol)
+# 			return sum(sol)
 
 
-solution = Solution()
+# solution = Solution()
+
+# Nested List Weight Sum II
+# Time(D) D = Depth of Nested Lists
+# Time(N) N = Number of items in List
+
+class Solution(object):
+	def depthSumInverse(self, nestedList):
+		return self.calculateSum(nestedList, self.getMaxDepth(nestedList))
+
+	def getMaxDepth(self, nestedList):
+		depth_value = 0
+		for value in range(nestedList):
+			if not value.isInteger():
+				depth_value = max(depth_value, self.getMaxDepth(value.getList()))
+		
+		return depth_value + 1 
+
+	def calculateSum(self, nestedList, depth):
+		sum = 0
+
+		for value in range(nestedList):
+			if value.isInteger():
+				sum += value.getInteger() * depth
+			else:
+				sum += self.calculateSum(value.getList(), depth -1)
--- _Shortest Word Distance II/ShortestWordDistanceII.py
+++ _Shortest Word Distance II/ShortestWordDistanceII.py
@@ -1,30 +1,41 @@
 
 class WordDistance(object):
 	def __init__(self, words):
-		self.dic, self.l = {}, len(words)
-		for i, w in enumerate(words):
-			self.dic[w] = self.dic.get(w, []) + [i]
+		# Interate over the wordList and mapping a word to all its locations in the array
+		self.wordsDictionary = {}
+		self.wordListLength = len(words)
 
-	# @param {string} word1
-	# @param {string} word2
-	# @return {integer}
-	# Adds a word into the data structure.
+		for idx, word in enumerate(words):
+			self.wordsDictionary[word] = self.wordsDictionary.get(word, []) + [idx]
+		
 
-	def shortest(self, word1, word2):
-		l1, l2 = self.dic[word1], self.dic[word2]
-		i = j = 0
-		res = self.l
-		# O(m+n) time complexity
-		#f or constructor is O(n). n is the length of words list
-		# for shortest() is O(k), k is the total frequency of word1 and word2
-		while i < len(l1) and j < len(l2):
-			res = min(res, abs(l1[i]-l2[j]))
-			if l1[i] < l2[j]:
+	def shortest_path(self, word1, word2):
+		first_word = self.wordsDictionary[word1]
+		second_word = self.wordsDictionary[word2]
+		i = 0
+		j = 0
+		result = self.wordListLength
+
+		while i < len(first_word) and j < len(second_word):
+			result = min(result, abs(first_word[i] - second_word[j]))
+			if first_word[i] < second_word[j]:
 				i += 1
-			else:
+			else: 
 				j += 1
-		return res
+
+			return result
 
 
 wordDistance = WordDistance(["practice", "makes", "perfect", "coding", "makes"])
-print(wordDistance.shortest("coding", "practice"))
+print(wordDistance.shortest_path("coding", "practice"))
+
+# Algorithm
+
+# In the constructor of the class, we simply iterate over the given list of words and prepare a dictionary, mapping a word to all it's locations in the array.
+# Since we process all the words from left to right, we will get all the indices in a sorted order by default for all the words. So, we don't have to sort the indices ourselves.
+# Let's call the dictionary that we build, locations.
+# For a given pair of words, obtain the list of indices (appearances inside the original list/array of words). Let's call the two arrays loc1 and loc2.
+# Initialize two pointer variables l1 = 0 and l2 = 0.
+# For a given l1 and l2, we first update (if possible) the minimum difference (distance) till now i.e. dist = min(dist, abs(loc1[l1] - loc2[l2])). Then, we check if loc1[l1] < loc2[l2] and if this is the case, we move l1 one step forward i.e. l1 = l1 + 1. Otherwise, we move l2 one step forward i.e. l2 = l2 + 1.
+# We keep doing this until all the elements in the smaller of the two location arrays are processed.
+# Return the global minimum distance between the words.
