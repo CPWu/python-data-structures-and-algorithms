e4d97f556ef1a1c99371c075e7c3384044e23189
Chun Wu <the.chun.wu@gmail.com>
2021-10-14 10:01:11 -0400

    coded min and max approach to selection sort

--- Sorting/Selection Sort/SelectionSort.py
+++ Sorting/Selection Sort/SelectionSort.py
@@ -1,21 +1,4 @@
-def selectionSort(array):
-	for fillSlot in range(len(array)-1, 0, -1):
-		position_of_max = 0
-		
-		for index in range(1, fillSlot + 1):
-			if array[index] > array[position_of_max]:
-				position_of_max = index
-		temp = array[fillSlot]
-		array[fillSlot] = array[position_of_max]
-		array[position_of_max] = temp
-	return array
-
-
-array = [5,8,3,10,1]
-
-selectionSort(array)
-
-print(array)
+## Brute Force / Decrease and Conquer Approach ##
 
 # Implemented Using Max Value, Can be done Min (Move Item Left) or Max (Move Item Right)
 def selection_sort(array):
@@ -23,8 +6,48 @@ def selection_sort(array):
 	# range(start_param, stop_param, step_param), only stop_param is required
 	
 	# Loop will start with length of array and step down 1 at time until first element in array.
+	# move_value indicates how many items were sorted
 	for move_value in range(len(array)-1, 0, -1):
+		# To find the maximum value value of unsorted segment
+		# We first assume that the first element is the largest
 		position_of_max = 0
 		
+		# We use index to loop through the remaining elements
 		for index in range(1, move_value + 1):
-			if arrayp[inde
+		# Update the position_of_max if the element at index is lower than it.
+			if array[index] > array[position_of_max]:
+				position_of_max = index
+		# After finding the largest value of the unsorted regions, swap with the first unsorted element.		
+		temp = array[move_value]
+		array[move_value] = array[position_of_max]
+		array[position_of_max] = temp
+		print(array)
+	return array
+
+# # Implemented Using Min Value, Can be done Min (Move Item Left) or Max (Move Item Right)
+# def selection_sort(array):
+# 	# Use Python Range Function
+# 	# range(start_param, stop_param, step_param), only stop_param is required
+	
+# 	# Loop will start with length of array and step down 1 at time until first element in array.
+# 	# move_value indicates how many items were sorted
+# 	for move_value in range(len(array)-1):
+# 		# To find the minimum value value of unsorted segment
+# 		# We first assume that the first element is the lowest
+# 		position_of_min = 0
+# 		# We use j to loop through the remaining elements
+# 		for j in range(move_value+1, len(array)):
+# 			# Update the position_of_min if the element at j is lower than it.
+# 			if array[j] < array[position_of_min]:
+# 				position_of_min = j
+# 			# After finding the lowest value of the unsorted regions, swap with the first unsorted element.
+# 			temp = array[move_value]
+# 			array[move_value] = array[position_of_min]
+# 			array[position_of_min] = temp
+# 			print(array)
+# 	return array
+
+array = [5,8,3,10,1]
+selection_sort(array)
+
+print(array)
