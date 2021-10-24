dcaf6df43995cc52a3b12cc9d1fa5c1c1778b98e
Chun Wu <the.chun.wu@gmail.com>
2021-10-24 13:20:59 -0400

    recursion - string permutation

--- Recursion/String Permutation/README.md
+++ Recursion/String Permutation/README.md
@@ -4,5 +4,9 @@ Goal: Use Python to implement String Permutation
 
 ## Overview
 
+Given a string, write a function that uses recursion to output a list of all possible permutations of that string. For example, given s='abc' the function should return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+Note: If a character is repeated, treat each occurence as distinct, for example an input of 'xxx' would return a list with 6 "versions" of 'xxx'
+
 
 ## See Also
--- Recursion/String Permutation/String Permutation.py
+++ Recursion/String Permutation/String Permutation.py
@@ -1,9 +1,21 @@
-def reverse_string(string):
-    # Base Case
-    if len(string) <= 1:
-        return string
-    # Recursively call function until we hit base case.   
-    else:
-        return reverse_string(string[1:]) + string[0]
+# Iterate through the initial string – e.g., ‘abc’.
+# For each character in the initial string, set aside that character and get a list of all permutations of the string that’s left. So, for example, if the current iteration is on 'b', we’d want to find all the permutations of the string 'ac'.
+# Once you have the list from step 2, add each element from that list to the character from the initial string, and append the result to our list of final results. So if we’re on 'b' and we’ve gotten the list ['ac', 'ca'], we’d add 'b' to those, resulting in 'bac' and 'bca', each of which we’d add to our final results.
+# Return the list of final results.
+def permute(s):
+    out = []
 
-print(reverse_string("hello world"))
+    # Base Case
+    if len(s) == 1:
+        out = [s]
+    else:
+        # For every letter in string
+        for i, let in enumerate(s):
+            # For every permutatioon resulting from Step 2 and 3 described above
+            for perm in permute(s[:i] + s[i+1:]):
+                # Add it to output
+                out += [let + perm]
+
+    return out
+
+print(permute('abc'))
